f31ae65ed9370d1d203291e1d0e0b5fae1c35ca8
zahidmehmood <zahid.mehmood@uhd.edu.iq>
2021-12-09 19:35:34 +0500

    pos tax report for odoo 15

---
+++ __manifest__.py
@@ -0,0 +1,44 @@
+{
+    'name': 'Saudia Tax Invoice for POS فاتورة ضريبية مبسطة',
+    'version': '15.0.0.0',
+    'category': 'Sales/Point of Sale',
+    'sequence': 40,
+    'summary': 'This Module will help in printing the Simplified Tax Report for POS فاتورة ضريبية مبسطة',
+    'description': """ Simplified Tax Report for POS
+    Point of Sale tax report for saudi arabia
+    Simplified VAT Report
+    Saudi Arabia VAT Invoice
+    VAT E-Invoice Standard
+    E-Invoicing
+    B2C VAT Invoice
+    Fatora 
+    فاتورة ضريبیة
+    فاتورة ضريبية مبسطة
+    """,
+    'license': 'OPL-1',
+
+    # Author
+    'author': 'Mediod Consulting Pvt. Ltd.',
+    'website': 'http://www.mediodconsulting.com/',
+    'maintainer': 'Mediod Consulting Pvt. Ltd.',
+    'depends': ['point_of_sale'],
+    'data': [
+        'views/assets.xml',
+    ],
+    'assets': {
+        'point_of_sale.assets': [
+            'pos_reports/static/src/js/lib/qrcode.min.js',
+            'pos_reports/static/src/js/lib/base64.js',
+        ],
+        'web.assets_qweb': [
+            'pos_reports/static/src/xml/OrderReceipt.xml',
+        ],
+    },
+    'data': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+    'price': 15.00,
+    'currency': 'USD',
+}
